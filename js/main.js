8af669f8a37c423dae1258e264b36752b2720380
StefanoYZ <ayepezz1@upao.edu.pe>
2025-06-22 20:37:34 -0500

    prueba frpmt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-// Genera radios del 1 al 10 dinámicamente
 function crearRadios(nombreCampo) {
     const contenedor = document.querySelector(`.radio-group[data-name='${nombreCampo}']`);
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 5; i++) {
         const radio = document.createElement("input");
         radio.type = "radio";
         radio.name = nombreCampo;
@@ -17,6 +16,7 @@ function crearRadios(nombreCampo) {
         contenedor.appendChild(label);
     }
 }
+
 
 // Campos tipo escala
 const escalas = [
@@ -49,7 +49,12 @@ document.getElementById("predict-form").addEventListener("submit", async functio
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+                // Invertir Job Satisfaction
+        if (jsonData["Job Satisfaction"]) {
+            jsonData["Job Satisfaction"] = 6 - parseInt(jsonData["Job Satisfaction"]);
+        }
+
+        const response = await fetch("https://api-ml-m9js.onrender.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(jsonData)
@@ -58,12 +63,23 @@ document.getElementById("predict-form").addEventListener("submit", async functio
         const resultadoDiv = document.getElementById("resultado");
         if (response.ok) {
             const data = await response.json();
-            resultadoDiv.innerHTML = `
-                <div class="result-box">
-                    <p><strong>¿Tiene depresión?</strong> ${data.tiene_depresion ? 'Sí' : 'No'}</p>
-                    <p><strong>Probabilidad:</strong> ${data.probabilidad}</p>
-                </div>
+            let resultadoHTML = `
+            <div class="result-box">
+              <p><strong>¿Tiene depresión?</strong> ${data.tiene_depresion ? 'Sí' : 'No'}</p>
+              <p><strong>Probabilidad:</strong> ${(data.probabilidad * 100).toFixed(2)}%</p>
+          `;
+          
+          if (data.tiene_depresion) {
+            resultadoHTML += `
+              <button onclick="location.href='consejos.html'" class="start-button" style="margin-top: 1rem;">
+                Ver Consejos
+              </button>
             `;
+          }
+          
+          resultadoHTML += '</div>';
+          resultadoDiv.innerHTML = resultadoHTML;
+          
         } else {
             const error = await response.json();
             resultadoDiv.innerHTML = `<p class="error">Error: ${error.error}</p>`;
